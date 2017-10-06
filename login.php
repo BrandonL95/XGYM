0a0288d11c6c41c8e0bb38ac6644af86c8a121fa
Deon <Gideondeonjacobus@Gmail.com>
2017-10-06 21:07:24 +0200

    Update login.php

--- login.php
+++ login.php
@@ -5,6 +5,8 @@ $user_pass = $_POST["password"];
 $mysql_qry = "select * from employee_data where username like '$user_name' and password like '$user_pass';";
 $result = mysqli_query($conn, $mysql_qry);
 if(mysqli_num_rows($result) > 0){
+	$row = mysqli_fetch_assoc($result);
+	$name =$row["name"];
 	echo "Login success";
 }
 else{
