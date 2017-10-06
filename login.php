ec3f4cbbe9a583dd85d8ca1295ae71cd2c977be9
Deon <Gideondeonjacobus@Gmail.com>
2017-10-06 22:01:53 +0200

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 require "connection.php";
 $user_name = $_POST["user_name"];
 $user_pass = $_POST["password"];
-$mysql_qry = "select * from employee where username like '$user_name' and password like '$user_pass';";
+$mysql_qry = "select * from employee11.employee where username like '$user_name' and password like '$user_pass';";
 $result = mysqli_query($conn, $mysql_qry);
 if(mysqli_num_rows($result) > 0){
 	
