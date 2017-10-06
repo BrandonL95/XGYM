9d050dcf2e462e9b366c12abb19439a94f714ed6
Deon <Gideondeonjacobus@Gmail.com>
2017-10-06 22:12:56 +0200

    Update login.php

--- login.php
+++ login.php
@@ -11,6 +11,6 @@ if(mysqli_num_rows($result) > 0){
 else{
 	$row = mysqli_fetch_assoc($result);
 	$name =$row["name"];
-	echo "Login not successfullll " . $result;
+	echo "Login not successfullll " . $user_name;
 }
 ?>
