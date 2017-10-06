8fcf8b07a8e42fb768071348bf664a3f720f375e
Deon <Gideondeonjacobus@Gmail.com>
2017-10-06 20:33:53 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$db_name = "employee101";
-$mysql_username = "root";
-$mysqpl_password = "";
-$server_name = "localhost";
+$db_name = "employee11";
+$mysql_username = "employee11";
+$mysqpl_password = "employee1.";
+$server_name = "mysql5.gear.host";
 $conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $db_name);
 
 ?>
