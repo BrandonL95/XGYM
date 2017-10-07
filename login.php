72bf6959ec756b6b17218a1c4bdcdfe939867fa5
Deon <Gideondeonjacobus@Gmail.com>
2017-10-07 12:03:45 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,7 @@
 <?php
 require "connection.php";
 $myname = "Deon";
-$ran = mysql_query("SELECT name FROM employee_data WHERE name = '$myname'");
-$res = mysql_fetch_array($ran);
-$user_name = $_POST["username"];
+$user_name = $_POST["user_name"];
 $user_pass = $_POST["password"];
 $mysql_qry = "select * from employee_data where username like '$user_name' and password like '$user_pass';";
 $result = mysqli_query($conn, $mysql_qry);
@@ -12,8 +10,6 @@ if(mysqli_num_rows($result) > 0){
 	echo "Login success";
 }
 else{
-	$row = mysqli_fetch_assoc($result);
-	$name =$row["name"];
-	echo $res['name'];
+	echo $user_name;
 }
 ?>
